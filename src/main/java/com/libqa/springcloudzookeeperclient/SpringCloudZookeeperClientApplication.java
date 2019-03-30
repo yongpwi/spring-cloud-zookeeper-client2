e9ed89beb06e263bc0a341451175f210a9dde2ad
yongpwi <seeds.pear.jr@gmail.com>
2019-03-30 22:24:53 +0900

    init project of spring-cloud-zookeeper-client2

--- src/main/java/com/libqa/springcloudzookeeperclient/SpringCloudZookeeperClientApplication.java
+++ src/main/java/com/libqa/springcloudzookeeperclient/SpringCloudZookeeperClientApplication.java
@@ -6,14 +6,11 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
@@ -26,12 +23,6 @@ public class SpringCloudZookeeperClientApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCloudZookeeperClientApplication.class, args);
 	}
-
-    @LoadBalanced
-    @Bean
-    RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
 
     @RestController
     class ServiceInstanceRestController {
@@ -48,6 +39,11 @@ public class SpringCloudZookeeperClientApplication {
         @GetMapping("/howling")
         public String howling() {
             return "howling-8081~!!!";
+        }
+
+        @GetMapping("/product")
+        public String product() {
+            return "howling-product-8081";
         }
     }
 }
